15946cc29e9ab2f4553b3aa64df54a58a03a1b94
Tatiana Gasilina <ernolova@gmail.com>
2024-12-26 14:36:03 +0300

    add tests for reports

---
+++ src/test/java/hflabs/SelenideTest.java
@@ -0,0 +1,29 @@
+package hflabs;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+
+    @Test
+    public void testIssueSearch() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $(".header-search-button").click();
+        $("#query-builder-test").sendKeys("tatianagas/qa_guru_HW9");
+        $("#query-builder-test").submit();
+
+        $(linkText("tatianagas/qa_guru_HW9")).click();
+        $("#issues-tab").click();
+        $(withText("#1")).should(Condition.exist);
+    }
+}
